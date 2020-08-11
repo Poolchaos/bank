71e4201d2ce145cec8d09427b42d9d2eaff5443b
Phillip-Juan van der Berg <phillipjuanvanderberg@gmail.com>
2020-08-11 09:54:40 +0200

    chore(setup): create base application

---
+++ README.md
@@ -0,0 +1,19 @@
+# Bank
+
+### Description
+This app showcases 2 implemented features:
+-  As a User, I want to be able to see my profile on the sidebar of the dashboard when I login
+-  As a User, I always want to see the sidebar visible through different pages of the dashboard
+
+---
+## Installation and Usage
+Prerequisite
+- `yarn` is required to install third party libraries
+
+### Setup
+- Clone the app: $ `git clone https://github.com/Poolchaos/bank`
+- Change directory into the app: $ `cd bank`
+- Install dependencies:  $ `yarn install`
+- Now start up the server: $ `yarn start`
+- Launch the app in a browser via url: `localhost:3000`
+
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(t|j)sx?$/,
+        use: { loader: 'awesome-typescript-loader' },
+        exclude: path.resolve(__dirname, "node_modules")
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx", ".ts", ".tsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
